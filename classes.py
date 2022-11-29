70a847fa70a99cb38bcda71c2f625225503f5aca
LacertaKate <112935023+LacertaKate@users.noreply.github.com>
2022-11-29 12:10:15 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -39,3 +39,78 @@ class Photos(Base):
     count_likes = sq.Column(sq.Integer)
     id_dating_user = sq.Column(sq.Integer, sq.ForeignKey('dating_user.id', ondelete='CASCADE'))
     
+    
+def delete_db_favorites(ids):
+    current_user = session.query(DatingUser).filter_by(vk_id=ids).first()
+    session.delete(current_user)
+    session.commit()
+
+def check_db_master(ids):
+    current_user_id = session.query(User).filter_by(vk_id=ids).first()
+    return current_user_id
+
+def check_db_user(ids):
+    dating_user = session.query(DatingUser).filter_by(
+        vk_id=ids).first()
+    blocked_user = session.query(BlackList).filter_by(
+        vk_id=ids).first()
+    return dating_user, blocked_user
+  
+ def check_db_favorites(ids):
+    current_users_id = session.query(User).filter_by(vk_id=ids).first()
+    alls_users = session.query(DatingUser).filter_by(id_user=current_users_id.id).all()
+    return alls_users
+
+def write_msg(user_id, message, attachment=None):
+    vk.method('messages.send',
+              {'user_id': user_id,
+               'message': message,
+               'random_id': randrange(10 ** 7),
+               'attachment': attachment})
+def register_user(vk_id):
+    try:
+        new_user = User(
+            vk_id=vk_id
+        )
+        session.add(new_user)
+        session.commit()
+        return True
+    except (IntegrityError, InvalidRequestError):
+        return False
+ 
+def add_user(event_id, vk_id, first_name, second_name, city, link, id_user):
+    try:
+        new_user = DatingUser(
+            vk_id=vk_id,
+            first_name=first_name,
+            second_name=second_name,
+            city=city,
+            link=link,
+            id_user=id_user
+        )
+        session.add(new_user)
+        session.commit()
+        write_msg(event_id,
+                  'Успешно добавлен в избранное')
+        return True
+    except (IntegrityError, InvalidRequestError):
+        write_msg(event_id,
+                  'Пользователь уже в избранном.')
+        return False
+
+def add_user_photos(event_id, link_photo, count_likes, id_dating_user):
+    try:
+        new_user = Photos(
+            link_photo=link_photo,
+            count_likes=count_likes,
+            id_dating_user=id_dating_user
+        )
+        session.add(new_user)
+        session.commit()
+        write_msg(event_id,
+                  'Фото пользователя сохранено в избранном')
+        return True
+    except (IntegrityError, InvalidRequestError):
+        write_msg(event_id,
+                  'Невозможно добавить фото этого пользователя(Уже сохранено)')
+        return False
