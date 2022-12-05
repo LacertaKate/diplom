d43db1539f2e0d807b0ce2a3b752681d15e4dd56
LacertaKate <112935023+LacertaKate@users.noreply.github.com>
2022-12-05 11:13:08 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,12 +1,11 @@
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
-from vk_tokens import group_token
-from functions import write_msg
-from random import randrange
 import sqlalchemy as sq
+import vk_api
+from sqlalchemy.exc import IntegrityError, InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import IntegrityError, InvalidRequestError
+from vk_api.longpoll import VkLongPoll
+from functions import write_msg
+from vk_tokens import group_token
 
 Base = declarative_base()
 engine = sq.create_engine('postgresql://user@localhost:5432/vkinder_db',
@@ -18,10 +17,12 @@ longpoll = VkLongPoll(vk)
 session = Session()
 connection = engine.connect()
 
+
 class User(Base):
     __tablename__ = 'user'
     id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
     vk_id = sq.Column(sq.Integer, unique=True)
+
 
 class DatingUser(Base):
     __tablename__ = 'dating_user'
@@ -33,28 +34,33 @@ class DatingUser(Base):
     link = sq.Column(sq.String)
     id_user = sq.Column(sq.Integer, sq.ForeignKey('user.id', ondelete='CASCADE'))
 
+
 class Photos(Base):
     __tablename__ = 'photos'
     id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
     link_photo = sq.Column(sq.String)
     count_likes = sq.Column(sq.Integer)
     id_dating_user = sq.Column(sq.Integer, sq.ForeignKey('dating_user.id', ondelete='CASCADE'))
-    
-    
+
+
 def delete_db_favorites(ids):
     current_user = session.query(DatingUser).filter_by(vk_id=ids).first()
     session.delete(current_user)
     session.commit()
 
+
 def check_db_master(ids):
     return session.query(User).filter_by(vk_id=ids).first()
 
+
 def check_db_user(ids):
     return session.query(DatingUser).filter_by(vk_id=ids).first()
-  
+
+
 def check_db_favorites(ids):
     current_users_id = session.query(User).filter_by(vk_id=ids).first()
     return session.query(DatingUser).filter_by(id_user=current_users_id.id).all()
+
 
 def register_user(vk_id):
     try:
@@ -66,7 +72,8 @@ def register_user(vk_id):
         return True
     except (IntegrityError, InvalidRequestError):
         return False
- 
+
+
 def add_user(event_id, vk_id, first_name, second_name, city, link, id_user):
     try:
         new_user = DatingUser(
@@ -87,6 +94,7 @@ def add_user(event_id, vk_id, first_name, second_name, city, link, id_user):
                   'Пользователь уже в избранном.')
         return False
 
+
 def add_user_photos(event_id, link_photo, count_likes, id_dating_user):
     try:
         new_user = Photos(
@@ -103,6 +111,7 @@ def add_user_photos(event_id, link_photo, count_likes, id_dating_user):
         write_msg(event_id,
                   'Уже сохранено в избранном')
         return False
-      
+
+
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
