b986dcd744efca5619c9dd0da5cb3f33f5442546
LacertaKate <112935023+LacertaKate@users.noreply.github.com>
2022-12-05 11:11:27 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,24 +1,23 @@
-from random import randrange
-import random
-import vk_api
-import json
 import datetime
-from vk_api.longpoll import VkLongPoll, VkEventType
-from vk_tokens import group_token, user_token, V
+import json
+from random import randrange
+import vk_api
 from vk_api.exceptions import ApiError
-from sqlalchemy.exc import IntegrityError, InvalidRequestError
+from vk_api.longpoll import VkLongPoll
+from vk_tokens import group_token, user_token, V
 
 vk_session = vk_api.VkApi(token=user_token)
 vk_session2 = vk_api.VkApi(token=group_token)
 longpoll = VkLongPoll(vk_session)
 
+
 def get_user_info(user_id):
     user_info = {}
     try:
-        if response := vk_session.method('users.get', 
-                                                    {'user_id': user_id, 
-                                                    'v': 5.131, 
-                                                    'fields': 'sex, status, age_at, age_to, has_photo, count, hometown'}):
+        if response := vk_session.method('users.get',
+                                         {'user_id': user_id,
+                                          'v': 5.131,
+                                          'fields': 'sex, status, age_at, age_to, has_photo, count, hometown'}):
             for key, value in response[0].items():
                 user_info[key] = value['id'] if key == 'city' else value
         else:
@@ -30,6 +29,7 @@ def get_user_info(user_id):
         print(f'Error! {e}')
     return user_info
 
+
 def write_msg(user_id, message, attachment='0'):
     vk_session.method('messages.send', {'user_id': user_id,
                                         'message': message,
@@ -40,15 +40,15 @@ def write_msg(user_id, message, attachment='0'):
 def find_matches(user_info):
     try:
         response = vk_session2.method('users.search', {
-                                      'age_from': user_info['age'] - 3,
-                                      'age_to': user_info['age'] + 3,
-                                      'sex': 3 - user_info['sex'],
-                                      'city': user_info['city'],
-                                      'city_id': user_info['city'],
-                                      'status': 6,
-                                      'has_photo': 1,
-                                      'count': 1000,
-                                      'v': 5.131})
+            'age_from': user_info['age'] - 3,
+            'age_to': user_info['age'] + 3,
+            'sex': 3 - user_info['sex'],
+            'city': user_info['city'],
+            'city_id': user_info['city'],
+            'status': 6,
+            'has_photo': 1,
+            'count': 1000,
+            'v': 5.131})
         if response:
             if response.get('items'):
                 return response.get('items')
@@ -59,6 +59,7 @@ def find_matches(user_info):
     except vk_api.exceptions.ApiError as e:
         write_msg(user_info['id'], '''Извините, что-то пошло не так.''')
         print(f'Error! {e}')
+
 
 def get_photo(user_id):
     vk_ = vk_api.VkApi(token=user_token)
@@ -85,9 +86,11 @@ def get_photo(user_id):
             users_photos.append(['нет фото.'])
     return users_photos
 
+
 def sort_likes(photos):
     result = [element for element in photos if element != ['нет фото.'] and photos != 'нет доступа к фото']
     return sorted(result)
+
 
 def json_create(lst):
     today = datetime.date.today()
@@ -99,6 +102,7 @@ def json_create(lst):
     with open("result.json", "a", encoding='UTF-8') as write_file:
         json.dump(res_list, write_file, ensure_ascii=False)
     print('Информация о загруженных файлах успешно записана в json файл.')
+
 
 def _extracted_from_json_create_(today_str, res, info, res_list):
     res['data'] = today_str
