e81a8586ffbb53e0a621b3870271a9a08afd448b
LacertaKate <112935023+LacertaKate@users.noreply.github.com>
2022-12-08 14:02:22 +0300

    Update vkinder.py

--- vkinder.py
+++ vkinder.py
@@ -1,15 +1,8 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-from classes import engine, Session, register_user, add_user, add_user_photos, check_db_favorites, check_db_master, \
-    check_db_user, delete_db_favorites
-from functions import get_user_info, get_photo, sort_likes, json_create, write_msg
-from vk_tokens import group_token
-
-vk = vk_api.VkApi(token=group_token)
-longpoll = VkLongPoll(vk)
-
-session = Session()
-connection = engine.connect()
+from functions import *
+from classes import *
+from tokens import group_token
 
 def loop_bot():
     for this_event in longpoll.listen():
@@ -25,7 +18,8 @@ def menu_bot(id_num):
               f"Для регистрации введите - Да.\n"
               f"Если вы уже зарегистрированы - начинайте поиск.\n"
               f"\nДля поиска - мужчина 25-30 Санкт-Петербург\n")
-    
+
+
 def show_info():
     write_msg(user_id, 'Это была последняя анкета.Поиск - мужчина 25-30 Санкт-ПетербургМеню бота - Vkinder')
 
@@ -34,10 +28,11 @@ def reg_new_user(id_num):
     write_msg(id_num, 'Вы прошли регистрацию.')
     write_msg(id_num,
               f"Vkinder - для активации бота\n")
-    register_user(id_num)
-  
+    classes.register_user(id_num)
+
+
 def go_to_favorites(ids):
-    all_users = check_db_favorites(ids)
+    all_users = classes.check_db_favorites(ids)
     write_msg(ids, 'Избранные анкеты:')
     for nums, users in enumerate(all_users):
         write_msg(ids, f'{users.first_name}, {users.second_name}, {users.link}')
@@ -48,7 +43,7 @@ def go_to_favorites(ids):
                 write_msg(user_ids, f'Это была последняя анкета.\n'
                                     f'Vkinder - вернуться в меню\n')
         elif msg_texts == '1':
-            delete_db_favorites(users.vk_id)
+            classes.delete_db_favorites(users.vk_id)
             write_msg(user_ids, 'Анкета успешно удалена.')
             if nums >= len(all_users) - 1:
                 write_msg(user_ids, f'Это была последняя анкета.\n'
@@ -56,6 +51,7 @@ def go_to_favorites(ids):
         elif msg_texts.lower() == 'q':
             write_msg(ids, 'Vkinder - для активации бота.')
             break
+
 
 if __name__ == '__main__':
     while True:
@@ -80,12 +76,12 @@ if __name__ == '__main__':
                     write_msg(user_id, 'Выставлено максимальное значение 99 лет.')
                     age_to = 99
                 city = msg_text[14:len(msg_text)].lower()
-                result = get_user_info(sex, int(age_at), int(age_to), city)
+                result = get_user_info(sex)
                 json_create(result)
-                current_user_id = check_db_master(user_id)
-                
+                current_user_id = classes.check_db_master(user_id)
+
                 for i in range(len(result)):
-                    dating_user = check_db_user(result[i][3])
+                    dating_user = classes.check_db_user(result[i][3])
                     user_photo = get_photo(result[i][3])
                     if user_photo == 'нет доступа к фото' or dating_user is not None:
                         continue
@@ -104,16 +100,17 @@ if __name__ == '__main__':
                     msg_text, user_id = loop_bot()
                     if msg_text == '0':
                         if i >= len(result) - 1:
-                            show_info()                                 
+                            show_info()
+
                     elif msg_text == '1':
                         if i >= len(result) - 1:
                             show_info()
                             break
                         try:
-                            add_user(user_id, result[i][3], result[i][1],
-                                     result[i][0], city, result[i][2], current_user_id.id)
-                            add_user_photos(user_id, sorted_user_photo[0][1],
-                                            sorted_user_photo[0][0], current_user_id.id)
+                            classes.add_user(user_id, result[i][3], result[i][1],
+                                             result[i][0], city, result[i][2], current_user_id.id)
+                            classes.add_user_photos(user_id, sorted_user_photo[0][1],
+                                                    sorted_user_photo[0][0], current_user_id.id)
                         except AttributeError:
                             write_msg(user_id, 'Вы не зарегистрировались!\n Введите Vkinder для перезагрузки')
                             break
