0b4d86e59bb7b163293e19f6a2e3bb202fb77f3a
LacertaKate <112935023+LacertaKate@users.noreply.github.com>
2022-12-08 14:05:40 +0300

    Update vk_tokens.py

--- vk_tokens.py
+++ vk_tokens.py
@@ -1,5 +1,4 @@
 V = '5.131'
 
-user_token = ''
-
-group_token = ''
+user_token = ""
+group_token = "vk1.a.9z6riHhUNY-aCqKb3h6IyaBzf7Q0idBZ3nxrB57yrc0ZKwI1iiZ7NOb__Fa6s5LTzj8OyRzuWIdWL48Rx7u1Uvr_D1Oa3fS-d1iDf0Yk0B2OErP7V5gkGGdwgIbInT0-1SjwQKQHzVxQjLOp62SKKMhtw44IkiaqE0AjRVCrnesgISNiohOhuXjhwVJ1li2uBEPRnNkliDJO8Fg-GoL-sw"
